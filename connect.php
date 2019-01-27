5d8b58acb8bf7d70c9978a45e78a11a7561f5e93
bonises <43809557+bonises@users.noreply.github.com>
2019-01-27 17:41:29 +0100

    testar

--- connect.php
+++ connect.php
@@ -1,7 +1,5 @@
 <?php
-	// $host = 'utb-mysql.du.se';$user = 'db30';$password = 'FJJAcyMU';$dbname = 'db30';
-	// $host = 'edu-mysql.du.se'; $user = 'h17jonbe';	$password = 'pTrFX4ZpukVuv4M6';	$dbname = 'db-h17jonbe';
-	$host = '127.0.0.1'; $user = 'root';	$password = '';	$dbname = 'candyland';
+	$host = '127.0.0.1'; $user = 'root'; $password = ''; $dbname = 'perkaholic';
 	// Set DSN(DATABASE SOURCE NAME)
 	$dsn = 'mysql:host='. $host .';dbname='.$dbname.';'.'PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8" '; 
 	//skapa en PDO instans
--- index.php
+++ index.php
@@ -1,3 +1,31 @@
-<?php
-echo 'hello world';
-?>
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+	<title>
+		Perkahloic
+	</title>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+	<meta charset="utf-8">
+	<meta name="robots" content="noindex, nofollow">
+	<meta name="Perkaholic">
+	<meta name="keywords" content="Perkaholic, sweet sweet superpower">
+	<link rel="icon" type="image/x-icon" href="img/favicon.ico">
+</head>
+<body>
+	<header id="" class="border">
+		
+	</header>
+	<nav id="" class="border">
+		
+	</nav>
+	<main id="" class="border">
+		
+	</main>
+	<aside id="" class="border">
+		
+	</aside>
+	<footer id="" class="border">
+		
+	</footer>
+</body>
+</html>
